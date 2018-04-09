5d828e5fd90e67e72edd7429bd2ca545a5879b7e
Roger Saner <roger@burningdog.co.za>
2018-04-09 17:51:41 +0200

    feat: first pass at adding all Material components and calcuting time remaining (with tests)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatProgressSpinnerModule, MatToolbarModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatButtonModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule
+    ],
       declarations: [
         AppComponent
       ],
@@ -13,6 +22,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+/*
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -24,4 +34,83 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
+*/
+
+  describe('should calculate the correct time', () => {
+
+    it('should return the correct time left', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      const secondsLeft = component.INITIAL_SECONDS;
+      let actual = component.showMinutesAndSeconds(secondsLeft);
+      expect(actual).toBe('25:00');
+
+      actual = component.showMinutesAndSeconds(0);
+      expect(actual).toBe('00:00');
+
+      actual = component.showMinutesAndSeconds(225);
+      expect(actual).toBe('03:45');
+
+      actual = component.showMinutesAndSeconds(613);
+      expect(actual).toBe('10:13');
+    });
+
+    it('should correctly calculate how many minutes are in a number of seconds', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      let actual = component.calculateMinutesLeftToDisplay(60);
+      expect(actual).toBe(1);
+
+      actual = component.calculateMinutesLeftToDisplay(90);
+      expect(actual).toBe(1);
+
+      actual = component.calculateMinutesLeftToDisplay(120);
+      expect(actual).toBe(2);
+
+      actual = component.calculateMinutesLeftToDisplay(0);
+      expect(actual).toBe(0);
+
+      actual = component.calculateMinutesLeftToDisplay(component.INITIAL_SECONDS);
+      expect(actual).toBe(25);
+
+      actual = component.calculateMinutesLeftToDisplay(-10);
+      expect(actual).toBe(0, 'should handle negative numbers');
+    });
+
+    it('should calculate the seconds left in a minute', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      let actual = component.calculateSecondsLeftInMinuteToDisplay(15);
+      expect(actual).toBe(15);
+
+      actual = component.calculateSecondsLeftInMinuteToDisplay(65);
+      expect(actual).toBe(5);
+
+      actual = component.calculateSecondsLeftInMinuteToDisplay(30);
+      expect(actual).toBe(30);
+
+      actual = component.calculateSecondsLeftInMinuteToDisplay(75);
+      expect(actual).toBe(15);
+
+      actual = component.calculateSecondsLeftInMinuteToDisplay(120);
+      expect(actual).toBe(0);
+
+      actual = component.calculateSecondsLeftInMinuteToDisplay(145);
+      expect(actual).toBe(25);
+
+      actual = component.calculateSecondsLeftInMinuteToDisplay(-10);
+      expect(actual).toBe(0, 'should handle negative numbers');
+    });
+
+    it('should add a zero to a single digit minute or second', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      let actual = component.addAZeroToSingleDigitaMinuteOrSecond(0);
+      expect(actual).toBe('00');
+
+      actual = component.addAZeroToSingleDigitaMinuteOrSecond(10);
+      expect(actual).toBe('10');
+    });
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  countdown = 45;
+  INITIAL_SECONDS = 25 * 60;
+  secondsLeft = this.INITIAL_SECONDS;
+  interval;
+
+  ngOnInit(): void {
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      this.countdown++;
+      this.secondsLeft--;
+    }, 1000);
+  }
+
+  pause() {
+    clearInterval(this.interval);
+  }
+
+  reset() {
+    this.countdown = 0;
+    this.secondsLeft = this.INITIAL_SECONDS;
+    clearInterval(this.interval);
+  }
+
+  showMinutesAndSeconds(seconds: number): string {
+    const minutesLeft = this.calculateMinutesLeftToDisplay(seconds);
+    const secondsLeft = this.calculateSecondsLeftInMinuteToDisplay(seconds);
+    const time = this.addAZeroToSingleDigitaMinuteOrSecond(minutesLeft) +
+                 ':' +
+                 this.addAZeroToSingleDigitaMinuteOrSecond(secondsLeft);
+    return time;
+  }
+
+  calculateMinutesLeftToDisplay(seconds: number): number {
+    const secondsAreNegative = seconds < 0;
+    if (secondsAreNegative) {
+      return 0;
+    }
+    return Math.floor(seconds / 60);
+  }
+
+  calculateSecondsLeftInMinuteToDisplay(seconds: number): number {
+    const secondsAreNegative = seconds < 0;
+    if (secondsAreNegative) {
+      return 0;
+    }
+    const minutesGoneByInSeconds = this.calculateMinutesLeftToDisplay(seconds) * 60;
+    const secondsRemaining = seconds - minutesGoneByInSeconds;
+    return secondsRemaining;
+  }
+
+  addAZeroToSingleDigitaMinuteOrSecond(time: number): string {
+    let newTime = time.toString();
+    const timeIsOnlySingleDigit = newTime.length === 1;
+    if (timeIsOnlySingleDigit) {
+      newTime = '0' + newTime;
+    }
+    return newTime;
+  }
 }
